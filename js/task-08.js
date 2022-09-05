948afe5440f53a41f133e3f5585b4219741e1b42
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-06 01:11:00 +0300

    add task 08-09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const refs = {
+  formRef: document.querySelector(".login-form"),
+  emailRef: document.querySelector("input[name=email]"),
+  passwordRef: document.querySelector("input[name=password]"),
+  btnRef: document.querySelector("button"),
+};
+// const emailRef = document.querySelector("input[name=email]");
+// console.log(emailRef);
+refs.formRef.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  if (email.value === "" || password.value === "") {
+    return window.alert("Все поля должны быть заполнены!");
+  }
+
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector(".change-color");
+const textColorRef = document.querySelector(".color");
+
+btnRef.addEventListener("click", onChangeColorBody);
+
+function onChangeColorBody(event) {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  textColorRef.textContent = `${randomColor}`;
+}
